d89c45837c4ad02351a686702057d0bfbeaf4b3a
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-03-09 14:21:11 +0530

    dummy validators added

--- PassengerReg.java
+++ PassengerReg.java
@@ -13,12 +13,29 @@ public class PassengerReg{
         // if data base is connected we need to confirm uniqueness of id
         return id;
     }
-    static boolean validateContact(StringBuffer contact){
-
+    static boolean validateContact(String contact){
+        if (contact.equals(" "))
+        return false;
         return true;
     }
-    static boolean validateEmail(StringBuffer email){
-
+    static boolean validateEmail(String email){
+        if (email.equals(" "))
+        return false;
+        return true;
+    }
+    static boolean validatePName(String name){
+        if (name.equals(" "))
+        return false;
+        return true;
+    }
+    static boolean validatePassword(String password){
+        if (password.equals(" "))
+        return false;
+        return true;
+    }
+    static boolean validateAddress(String address){
+        if (address.equals(" "))
+        return false;
         return true;
     }
 
@@ -26,21 +43,28 @@ public class PassengerReg{
         // System.out.println();
         Scanner sc = new Scanner(System.in);
         int pID = pIDGen();  // // A random 7 digit number for Passenger ID with a default value
-        String pName = sc.nextLine();  // //A string field for Passenger name(Maximum 50 characters)
-        StringBuffer emailb=new StringBuffer(" ");
-        while(!validateEmail(emailb)) 
-            emailb =new StringBuffer(sc.nextLine()); 
-        String email=emailb.toString(); // A //string field to capture the Email.
-        String password = sc.nextLine();  // A string field for Password(Maximum 30 characters)
-        String address = sc.nextLine();  // A string field to capture street, city details(Maximum 100 characters)
-        StringBuffer contactb=new StringBuffer(" "); ;
-        while(!validateContact(contactb)) 
-            contactb = new StringBuffer(sc.nextLine());
-        String contact=contactb.toString(); 
+        String pName = " ";
+        while(!validatePName(pName)) 
+            pName =sc.nextLine();
+        String email= " ";
+        while(!validateEmail(email)) 
+            email =sc.nextLine(); 
+        
+            String password = " ";
+            while(!validatePassword(password)) 
+                password = sc.nextLine();
+        String address = " ";
+        while(!validateAddress(address)) 
+            address =sc.nextLine();
+        String contact= " ";
+        while(!validateContact(contact)) 
+            contact= sc.nextLine();
+        // String contact=contactb.toString(); 
         // 
 
         Passenger p =new Passenger(pID, pName, email, password, address, contact);
         System.out.println(p);
+        sc.close();
     }
 
 }
